c285c72a8790871ed8fe3b58a5a8f561fbb401de
Jordy Aaldering <aalderingjordy@gmail.com>
2026-02-17 13:44:30 +0100

    hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> eframe::Result {
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        ..Default::default()
+    };
+
+    eframe::run_native(
+        "Energy Monitor",
+        options,
+        Box::new(|_cc| {
+            Ok(Box::<App>::default())
+        }),
+    )
+}
+
+struct App {
+    name: String,
+    age: u32,
+}
+
+impl Default for App {
+    fn default() -> Self {
+        Self {
+            name: "Arthur".to_owned(),
+            age: 42,
+        }
+    }
+}
+
+impl eframe::App for App {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("My egui Application");
+
+            ui.horizontal(|ui| {
+                let name_label = ui.label("Your name: ");
+                ui.text_edit_singleline(&mut self.name)
+                    .labelled_by(name_label.id);
+            });
+
+            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
+            if ui.button("Increment").clicked() {
+                self.age += 1;
+            }
+
+            ui.label(format!("Hello '{}', age {}", self.name, self.age));
+        });
+    }
 }
